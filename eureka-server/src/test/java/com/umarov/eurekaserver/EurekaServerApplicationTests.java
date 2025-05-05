be4dd6558bafe5faca03ff3b4ed3dbc34d9a02df
nodirbekUmarov04 <nadir.ymarov@gmail.com>
2025-05-05 13:14:59 +0500

    add eureka-server module

---
+++ eureka-server/src/test/java/com/umarov/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umarov.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
